420222a0b51b6103e1aabdd6a45470a487db32a7
pompo <txt39@yahoo.com>
2021-10-02 01:02:39 +0200

    login

--- Screens/Orders.js
+++ Screens/Orders.js
@@ -24,7 +24,7 @@ export default function Orders(props) {
   };
   return (
     <>
-      {arrayFood ? (
+      {total !== 0 ? (
         <View
           style={{
             backgroundColor: "white",
